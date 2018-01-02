425b73fbb073e57373b1abd6b243746017ec30ef
natsachaj <natascha_jakobsen@hotmail.com>
2018-01-02 15:07:39 +0100

    SOAP Consumer tilføjet (Virker) TCPClient Server (virker ikke helt endnu).

---
+++ SOAP_Movie_Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOAP_Movie_Consumer.MovieReference;
+
+namespace SOAP_Movie_Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Service1Client client = new Service1Client())
+            {
+                Movie[] ListOfMovies = client.GetMovie();
+                foreach (var mov in ListOfMovies)
+                {
+                    Console.WriteLine(mov.Titel + " " + mov.Rating);
+                }
+
+                
+
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TCPClient/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using SOAP_WCF_U_DB;
+
+namespace TCPClient
+{
+    class Client
+    {
+        //private readonly int PORT;
+
+        public Client(/*int port*/)
+        {
+            //this.PORT = port;
+        }
+
+
+
+
+
+        public void Start()
+        {
+            var film = new MovieReference.Service1Client();
+            //calc.GetVolume(20, 30, 10);
+            //calc.GetSide(2, 5, 4);
+
+            using (TcpClient client = new TcpClient("Localhost", 8080))
+            using (NetworkStream ns = client.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                sw.WriteLine(film.GetMovie());
+                
+                //sw.WriteLine(calc.GetVolume(50, 50, 10));
+                sw.Flush();
+
+                String incomingString = sr.ReadLine();
+                Console.WriteLine("Ekko er modtaget: " + incomingString);
+            }
+        }
+    }
+}
